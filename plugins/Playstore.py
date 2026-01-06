8855ac8b6d1ceb7922fdadfe025f6bd56eca2f91
serverofpakistan786-cpu <serverofpakistan786@gmail.com>
2026-01-06 19:36:25 +0530

    Update Playstore.py

--- plugins/Playstore.py
+++ plugins/Playstore.py
@@ -142,7 +142,7 @@ async def register_commands():
 
             # 4. Send
             caption = (
-                f"🎭 **Cipher Elite Play Store**\n\n"
+                f"🎭 **King ozix Play Store**\n\n"
                 f"📲 **App:** `{fullapp_name}`\n"
                 f"👨‍💻 **Developer:** [{dev_name}]({dev_link})\n"
                 f"⭐️ **Rating:** `{rating} ⭐`\n"
